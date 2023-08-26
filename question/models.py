2abcc7017fd7fb09610c38a3b41e66eb376d4faa
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-26 19:10:55 +0200

    DateTimeField question and answer classes in models

--- question/models.py
+++ question/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 class Question(models.Model):
     user = models.ForeignKey(User,related_name='Quetion_user',on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=120)
-    content = models.TextField(max_length=5000)
-    created_at = models.DecimalField(default=timezone.now)
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(default=timezone.now)
 
 
     def __str__(self) -> str:
@@ -15,8 +15,8 @@ class Question(models.Model):
 class Answer(models.Model):
     user = models.ForeignKey(User,related_name='Quetion_answer',on_delete=models.CASCADE)
     question = models.ForeignKey(Question,related_name='answer_question', on_delete=models.CASCADE)
-    answer = models.TextField(max_length=5000)
-    created_at = models.DecimalField(default=timezone.now)
+    answer = models.TextField(max_length=500)
+    created_at = models.DateTimeField(default=timezone.now)
 
 
     def __str__(self) -> str:
