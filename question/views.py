374afd00af546988ab7b2bb207f75fed20780a1a
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-29 18:36:44 +0200

    add def add_question views.py

--- question/views.py
+++ question/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Question
-
+from .forms import QuestionForm
 
 def question_list(request):
     data = Question.objects.all()
@@ -14,3 +14,16 @@ def question_list(request):
 def question_detail(request,question_id):
     data = Question.objects.get(id=question_id)
     return render(request,'question_detail.html',{'question':data})
+
+
+def add_question(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/question/')
+
+    else:
+        form = QuestionForm()
+
+    return render(request,'add.html',{'form':form})
