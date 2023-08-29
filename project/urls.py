5abe3fc4269967a21b11f30d53f351b300fe5d22
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-29 17:53:56 +0200

    add media and static urls.py

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from question.views import question_list ,question_detail
 
 urlpatterns = [
@@ -23,3 +25,7 @@ urlpatterns = [
     path('question/', question_list),
     path('question/<int:question_id>', question_detail),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
