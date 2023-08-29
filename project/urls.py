8892634d093ad7f62f57c87b041fa6d9ed546431
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-29 18:40:28 +0200

    add add_question in urls

--- project/urls.py
+++ project/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from question.views import question_list ,question_detail
+from question.views import question_list ,question_detail, add_question
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('question/', question_list),
+    path('question/add/', add_question),
     path('question/<int:question_id>', question_detail),
 ]
 
