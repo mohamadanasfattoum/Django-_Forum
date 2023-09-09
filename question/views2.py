0f2648e9e77eda50c45db6b87a9be1126beb4caf
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-09 13:52:34 +0200

    add # context in class Questionlist in views2 and question_list in loop in question_list.html

--- question/views2.py
+++ question/views2.py
@@ -1,6 +1,7 @@
 from django.views import generic
 from .models import Question
 
+
+# contexst: question_list or object_list Because we have many questions
 class Questionlist(generic.ListView):
     model = Question
-    
