927a47e31b3e65e946171b553ee5e0a60308291f
Mohamad anas fattoum <samaanas30@gmail.com>
2023-09-09 14:17:32 +0200

    add class Questiondetail in views2

--- question/views2.py
+++ question/views2.py
@@ -5,3 +5,7 @@ from .models import Question
 # contexst: question_list or object_list Because we have many questions
 class Questionlist(generic.ListView):
     model = Question
+
+
+class QuestionDetail(generic.DetailView):
+    pass
